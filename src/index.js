a3ee370c8b0fff464ea1647a41a785d5a74bd27f
Joel Guizar <joelguizar@gmail.com>
2018-01-24 23:36:28 -0800

    React Router initialized, Provider, browserHistory wrapper, Home Route

--- src/index.js
+++ src/index.js
@@ -5,11 +5,28 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+      <App />
+    </Router>
   </Provider>
   , document.querySelector('.container'));
+
+//cookies v. tokens
+
+/*cookies: automatically included on all requests,
+unique to each domain
+cannot be sent to different domains, so cant hijack other people sessions
+Headers - cookie: {}
+
+Tokens: have to manually wire, can be sent to any domain/multiple domain
+headers - authorization: asd2312d..
+-> way to go since API server could be from a different domain,
+and tokens can be cross-domain */
